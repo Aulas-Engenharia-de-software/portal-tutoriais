c657de3fc4cae90671fc408ebe0649cf6403e1ff
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2026-05-14 20:09:37 -0300

    feat: atualizacao tutorial-consumer-sqs.component.ts

--- src/app/features/tutorials/tutorial-consumer-sqs/tutorial-consumer-sqs.component.ts
+++ src/app/features/tutorials/tutorial-consumer-sqs/tutorial-consumer-sqs.component.ts
@@ -1,40 +1,41 @@
-import {Component} from '@angular/core';
-import {TutorialSidebarComponent} from '../../../shared/components/tutorial-sidebar/tutorial-sidebar.component';
-import {TutorialPhase} from '../../../shared/models/tutorial-phase.model';
-import {TutorialStepComponent} from '../../../shared/components/tutorial-step/tutorial-step.component';
-import {ZoomableImageComponent} from '../../../shared/components/zoomable-image/zoomable-image.component';
-import {CodeSnippetComponent} from '../../../shared/components/code-snippet/code-snippet.component';
-import {PhaseHeaderComponent} from '../../../shared/components/phase-header/phase-header.component';
+import { Component } from '@angular/core';
+import { TutorialSidebarComponent } from '../../../shared/components/tutorial-sidebar/tutorial-sidebar.component';
+import { TutorialPhase } from '../../../shared/models/tutorial-phase.model';
+import { TutorialStepComponent } from '../../../shared/components/tutorial-step/tutorial-step.component';
+import { CodeSnippetComponent } from '../../../shared/components/code-snippet/code-snippet.component';
+import { PhaseHeaderComponent } from '../../../shared/components/phase-header/phase-header.component';
 
 @Component({
   selector: 'app-tutorial-consumer-sqs',
   imports: [
-    TutorialSidebarComponent, TutorialStepComponent, ZoomableImageComponent, CodeSnippetComponent, PhaseHeaderComponent
+    TutorialSidebarComponent,
+    TutorialStepComponent,
+    CodeSnippetComponent,
+    PhaseHeaderComponent,
   ],
   templateUrl: './tutorial-consumer-sqs.component.html',
   styleUrl: './tutorial-consumer-sqs.component.css',
 })
 export class TutorialConsumerSqsComponent {
-
   tutorialTitle = 'Integração SQS';
   phases: TutorialPhase[] = [
     {
       id: 'introducao',
-      label: 'Introdução'
+      label: 'Introdução',
     },
     {
       id: 'dependencias',
-      label: 'Fase 1: Dependências'
+      label: 'Fase 1: Dependências',
     },
     {
       id: 'credenciais',
-      label: 'Fase 2: Credenciais'
+      label: 'Fase 2: Credenciais',
     },
     {
       id: 'adapter',
-      label: 'Fase 3: Adapter'
+      label: 'Fase 3: Adapter',
     },
-  ]
+  ];
 
   consumerCode = `
 @Component
@@ -55,7 +56,7 @@ public class SqsPedidoAdapter {;
         System.out.println("Mensagem consumida com sucesso.");
     };
 
-};`
+};`;
 
   applicationPropertiesKeys = `
 # Credenciais AWS
@@ -66,32 +67,31 @@ spring.cloud.aws.region.static=us-east-1
 # Nome da sua fila (Criada na Fase 2)
 queue.order-events=T0XN_SEU_NOME_PEDIDOS.fifo`
 
-
   sqsAwsDependency = `
 <dependency>
     <groupId>io.awspring.cloud</groupId>
     <artifactId>spring-cloud-aws-starter-sqs</artifactId>
-</dependency>`
+</dependency>`;
 
   springCloudAwsDepedencyManagement = `
-  <dependencyManagement>
-      <dependencies>
-         <dependency>
+<dependencyManagement>
+    <dependencies>
+        <dependency>
             <groupId>org.springframework.cloud</groupId>
             <artifactId>spring-cloud-dependencies</artifactId>
-            <version>2021.0.8</version>
+            <version>2025.0.0</version>
             <type>pom</type>
             <scope>import</scope>
-         </dependency>
-         <dependency>
+        </dependency>
+
+        <dependency>
             <groupId>io.awspring.cloud</groupId>
             <artifactId>spring-cloud-aws-dependencies</artifactId>
-            <version>3.0.2</version>
+            <version>3.4.0</version>
             <type>pom</type>
             <scope>import</scope>
-         </dependency>
-      </dependencies>
-   </dependencyManagement>
+        </dependency>
+    </dependencies>
+</dependencyManagement>
 `
-
 }
